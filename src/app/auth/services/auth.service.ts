91913b98412d69c33683805b6fdc68c18f45c89c
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-06-24 16:16:25 -0600

    Comenzando con AuthService, señales que contendrán los calores de el usario actual y su estatus de autenticación

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject( HttpClient );
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking );
+
+  login( email: string, password: string ): Observable<boolean> {
+    return of(true)
+  }
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,2 +1,2 @@
 // Este documento podría ser un .env, combinando con la generación automática de lo que requiera, eso lo hacemos dentro del archivo 'packeage.json'
-export const environment = {baseUrl: 'http://localhost:3000/auth/login'}
+export const environment = {baseUrl: 'http://localhost:3000'}
