0f1bb943e9ec358f5a8ff08bcd3a446df4182e0b
LeinardB92 <boanergespife@gmail.com>
2023-07-12 21:09:02 -0600

    actualización de paquetes con npm install

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,2 +1,2 @@
 // Este documento podría ser un .env, combinando con la generación automática de lo que requiera, eso lo hacemos dentro del archivo 'packeage.json'
-export const environment = {baseUrl: 'http://localhost:3000'}
+export const environment = {baseUrl: 'http://127.0.0.1:3000'}
