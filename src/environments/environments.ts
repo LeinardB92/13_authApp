d596fe55d959a4c7660306754c31cac1371475cd
LeinardB92 <boanergespife@gmail.com>
2023-07-18 19:24:30 -0600

    Configuración de variable de entorno e implementación de Hash en el router

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,2 +1,3 @@
 // Este documento podría ser un .env, combinando con la generación automática de lo que requiera, eso lo hacemos dentro del archivo 'packeage.json'
-export const environment = {baseUrl: 'http://127.0.0.1:3000'}
+// export const environment = {baseUrl: 'http://127.0.0.1:3000'}
+export const environment = {baseUrl: 'https://12nest-backend-production.up.railway.app'}
